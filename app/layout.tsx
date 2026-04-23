b2dcd9978a3204168c2bfd9623f9c30caca0b88a
Danny Andrews <dandini6713@gmail.com>
2026-04-23 09:48:19 +0100

    Commit local changes before merge

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en-GB">
+    <html lang="en-GB" suppressHydrationWarning>
       <body>{children}</body>
     </html>
   );
